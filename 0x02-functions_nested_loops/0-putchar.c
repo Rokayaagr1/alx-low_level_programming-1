f049d14b66d28146bc7c0198aa8821c7d2b36bd0
youssef1lam <youlam1998@gmail.com>
2023-03-21 11:30:13 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,12 @@
  * printMessage - Prints "_putchar"
  */
 void printMessage(void);
-
 int main(void)
 {
-    printMessage();
-    return (0);
+printMessage();
+return (0);
 }
-
 void printMessage(void)
 {
-    printf("_putchar\n");
+printf("_putchar\n");
 }
