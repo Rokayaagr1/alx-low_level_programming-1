084f00c2f2a04a7d20b66f19fffbf92c5100fd8a
youssef1lam <youlam1998@gmail.com>
2023-03-21 16:15:56 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,15 +6,8 @@
  * Return: last digit of number
  */
 
-int print_last_digit(int n)
+void print_last_digit(int n)
 {
-    int last_digit;
-
-    if (n < 0)
-        n = -n;
-
-    last_digit = n % 10;
-    _putchar(last_digit + '0');
-
-    return (last_digit);
+int last_digit = n % 10;
+printf("%d\n", last_digit);
 }
