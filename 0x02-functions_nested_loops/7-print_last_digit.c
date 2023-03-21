fe638f3e91ec6f74323a771d7dc335dc1b21ce07
youssef1lam <youlam1998@gmail.com>
2023-03-21 16:28:37 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,17 @@
  */
 int print_last_digit(int n)
 {
+int last_digit;
+
+if (n == INT_MIN)
+{
+last_digit = 8;
+_putchar(last_digit + '0');
+return last_digit;
+}
 if (n < 0)
-n *= -1;
-_putchar('0' + (n % 10));
-return (n % 10);
+n = -n;
+last_digit = n % 10;
+_putchar(last_digit + '0');
+return last_digit;
 }
