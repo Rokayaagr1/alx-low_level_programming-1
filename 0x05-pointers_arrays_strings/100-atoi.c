25be7310e4a7b8bf23706857ed2311a4f0a397f0
youssef1lam <youlam1998@gmail.com>
2023-03-27 08:41:55 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - convert a string to an integer
+ * @s: the string to reverse
+ */
+int _atoi(char *s)
+{
+    int sign = 1;
+    int result = 0;
+
+    while (*s)
+    {
+        if (*s == '-')
+            sign = -sign;
+        else if (*s >= '0' && *s <= '9')
+            result = result * 10 + (*s - '0');
+        else if (result > 0)
+            break;
+
+        s++;
+    }
+
+    return sign * result;
+}
