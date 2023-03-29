0773d0b4f4ef0504144b23fc41af4f07ec148242
youssef1lam <youlam1998@gmail.com>
2023-03-29 09:12:50 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,21 @@
 #include "main.h"
+
 /**
- *_strcmp - compares two strings.
- *@s1: pointer to first string.
- *@s2: pointer to second string.
- *
- *Return: value less than 0 if string is less than the other.
- *value greater than 0 if string is greater than the other.
- *0 if strings are equal.
+ * _strcmp - compare two strings
+ * @s1: one string
+ * @s2: one string
+ * Return: int that tells num spaces in between
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int counter, cmpVal;
+	int i = 0;
 
-	counter = 0;
-	while (s1[counter] == s2[counter] && s1[counter] != '\0')
-	{
-		counter++;
-	}
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
 
-	cmpVal = s1[counter] - s2[counter];
-	return (cmpVal);
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+	else
+		return (0);
 }
