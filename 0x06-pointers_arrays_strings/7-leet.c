e76bc5fde77fa4cd2b7abec7ec91d1dffac2aff0
youssef1lam <youlam1998@gmail.com>
2023-03-29 09:33:02 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,21 +12,20 @@
  */
 char *leet(char *s)
 {
-	char a[] = "aeotlAEOTL";
-	char n[] = "4307143071";
-	int i = 0;
-	int j;
-
-	while (*(s + i) != '\0')
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			if (*(s + i) == a[j])
-			{
-				*(s + i) = n[j];
-			}
-		}
-		i++;
-	}
-	return (s);
+char a[] = "aeotlAEOTL";
+char n[] = "4307143071";
+int i = 0;
+int j;
+while (*(s + i) != '\0')
+{
+for (j = 0; j <= 9; j++)
+{
+if (*(s + i) == a[j])
+{
+*(s + i) = n[j];
+}
+}
+i++;
+}
+return (s);
 }
