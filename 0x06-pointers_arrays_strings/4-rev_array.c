78230a354f183d7c136679deead78e7759c95a31
youssef1lam <youlam1998@gmail.com>
2023-03-29 09:13:42 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,22 @@
 #include "main.h"
+
 /**
- *reverse_array - revereses an array.
- *@a: pointer to array.
- *@n: number of elements of an array.
- *
- *Return: void.
+ * reverse_array - reverse array elements
+ * @a: array
+ * @n: number of elements in array
  */
+
 void reverse_array(int *a, int n)
 {
-	int temp, counter;
 
-	n = n - 1;
-	counter = 0;
-	while (counter <= n)
+	int tmp, beg = 0;
+	int end = n - 1; /* omit null terminator in length */
+
+	while (beg < end)
 	{
-		temp = a[counter];
-		a[counter++] = a[n];
-		a[n--] = temp;
+		tmp = *(a + beg);
+		*(a + beg) = *(a + end);
+		*(a + end) = tmp;
+		beg++, end--;
 	}
 }
