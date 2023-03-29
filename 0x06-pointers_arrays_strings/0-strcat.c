8e0b8396f4ce23aa9de77613e79c16502f7ebc4a
youssef1lam <youlam1998@gmail.com>
2023-03-29 09:02:14 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,19 +8,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+        int i = 0;
 	int j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
+	while (*(dest + i))
+	   i++;
+	while (*(src + j))
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+	   *(dest+i) = *(src+j);
+	   i++;
+	   j++;
 	}
-	dest[i] = '\0';
-
+	*(dest+i) = '\0';
 	return (dest);
 }
